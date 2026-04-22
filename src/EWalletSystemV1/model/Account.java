ee23c1155d21fe16a322aae498bbc5a5bfd50607
mostafa-talat72 <m.talat7274@gmail.com>
2026-04-22 14:44:33 +0200

    add EWallet System version1 task errasoft

--- src/EWalletSystemV1/model/Account.java
+++ src/EWalletSystemV1/model/Account.java
@@ -1,66 +1,69 @@
 package EWalletSystemV1.model;
 
+// Account class represents a user account in the E-Wallet system
 public class Account {
-    private String userName;
-    private String password;
-    private Double balance;
-    private String phoneNumber;
-    private float age;
+    // Fields (attributes of the account)
+    private String userName;     // Username of the account holder
+    private String password;     // Password for authentication
+    private Double balance;      // Current balance in the wallet
+    private String phoneNumber;  // Phone number of the account holder
+    private float age;           // Age of the account holder
 
-
+    // Default constructor
     public Account() {
     }
 
-
+    // Constructor for creating a new account with full details
     public Account(String userName, String password, String phoneNumber, float age) {
         this.setUserName(userName);
         this.setPassword(password);
-        this.setBalance(0.0);
+        this.setBalance(0.0); // New accounts start with balance = 0
         this.setPhoneNumber(phoneNumber);
         this.setAge(age);
     }
 
+    // Constructor for login (only username and password needed)
     public Account(String userName, String password) {
         this.setUserName(userName);
         this.setPassword(password);
     }
 
+    // Getter and Setter for userName
     public String getUserName() {
         return userName;
     }
-
     public void setUserName(String userName) {
         this.userName = userName;
     }
 
+    // Getter and Setter for password
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
 
+    // Getter and Setter for balance
     public Double getBalance() {
         return balance;
     }
-
     public void setBalance(Double balance) {
         this.balance = balance;
     }
 
+    // Getter and Setter for phoneNumber
     public String getPhoneNumber() {
         return phoneNumber;
     }
-
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
 
+    // Getter and Setter for age
     public float getAge() {
         return age;
     }
-
     public void setAge(float age) {
         this.age = age;
     }
--- src/EWalletSystemV1/model/EWalletSystem.java
+++ src/EWalletSystemV1/model/EWalletSystem.java
@@ -3,19 +3,26 @@ package EWalletSystemV1.model;
 import java.util.ArrayList;
 import java.util.List;
 
+// EWalletSystem class represents the overall wallet system
+// It stores the system name and manages a list of user accounts
 public class EWalletSystem {
+    // Constant system name (cannot be changed)
     private final String name = "EraaSoft Wallet";
 
+    // List to hold all accounts created in the system
     private List<Account> accounts = new ArrayList<>();
 
+    // Getter for system name
     public String getName() {
         return name;
     }
 
+    // Getter for accounts list
     public List<Account> getAccounts() {
         return accounts;
     }
 
+    // Setter for accounts list (allows replacing the entire list)
     public void setAccounts(List<Account> accounts) {
         this.accounts = accounts;
     }
