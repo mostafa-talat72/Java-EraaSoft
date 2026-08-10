1c645739a23ae7d1af03a697d885af1448527830
mostafa-talat72 <m.talat7274@gmail.com>
2026-08-10 17:36:29 +0300

    solve task lec2 Spring in eraasoft

--- src/Main.java
+++ src/Main.java
@@ -27,6 +27,14 @@ public class Main {
             System.out.println();
         }
     }
-
+    public int removeDuplicates(int[] nums) {
+        int i = 0, j =0;
+        while(j < nums.length){
+            if(nums[i] != nums[j])
+                nums[++i] = nums[j];
+            j++;
+        }
+        return i;
+    }
 
 }
