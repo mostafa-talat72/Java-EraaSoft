80d6aceaf5afb8b1344ea0264e5fa8a936bce9d2
mostafa-talat72 <m.talat7274@gmail.com>
2026-08-23 18:47:52 +0300

    solve task lec49 Hibarnate in eraasoft

--- src/Main.java
+++ src/Main.java
@@ -27,14 +27,60 @@ public class Main {
             System.out.println();
         }
     }
-    public int removeDuplicates(int[] nums) {
-        int i = 0, j =0;
-        while(j < nums.length){
-            if(nums[i] != nums[j])
-                nums[++i] = nums[j];
-            j++;
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int l = 0, r = citations.length, mid = 0, out = 0;
+        while(l<=r){
+            mid=(l+r) / 2;
+            System.out.println("mid = " + mid);
+            int lowBound = lower_bound(citations, mid);
+            if(citations.length -  lowBound >= mid ){
+                l = mid+1;
+                out = Math.max(out, mid);
+            }else
+                r=mid-1;
         }
-        return i;
+        return out;
+    }
+    public int lower_bound(int[] citations, int num){
+        int l = 0, r = citations.length, mid = 0, out = citations.length;
+        while(l<r){
+            mid=(l+r) / 2;
+            if(citations[mid] >= num){
+                r=mid-1;
+                out = mid;
+            }else
+                l = mid+1;
+        }
+        System.out.println("num = " + out);
+        return out;
+    }
+}
+class RandomizedSet {
+    private List<Integer> list;
+    public RandomizedSet() {
+        list = new ArrayList<>();
     }
 
+    public boolean insert(int val) {
+        if(list.contains(val))
+            return false;
+        list.add(val);
+       return true;
+    }
+
+    public boolean remove(int val) {
+        boolean flag = false;
+        if(list.contains(val))
+        {
+            list.remove((Integer) val);
+            flag = true;
+        }
+        return flag;
+    }
+
+    public int getRandom() {
+        int randIdx = new Random().nextInt(list.size());
+        return list.get(randIdx);
+    }
 }
